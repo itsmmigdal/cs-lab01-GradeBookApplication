fa234e9bf16153d812d303af18df31967a5f4e28
itslisek <80004169+itsmmigdal@users.noreply.github.com>
2022-04-03 16:09:08 +0200

    Create RankedGradeBook.cs

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name, bool isWeighted) : base(name, isWeighted)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5) throw new InvalidOperationException();
+
+            int betterStudentsCount = 0;
+            for (int i = 0; i < Students.Count; i++)
+            {
+                if (Students[i].AverageGrade > averageGrade) betterStudentsCount++;
+            }
+            double rank = (double)(betterStudentsCount+1) / Students.Count;
+
+            if (rank <= .2) return 'A';
+            else if (rank <= .4) return 'B';
+            else if (rank <= .6) return 'C';
+            else if (rank <= .8) return 'D';
+            else return 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5) Console.WriteLine("Ranked grading requires at least 5 students.");
+            else base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5) Console.WriteLine("Ranked grading requires at least 5 students.");
+            else base.CalculateStudentStatistics(name);
+        }
+    }
+}
