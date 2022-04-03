8ea2c02b591c48733915cec7810da57e46dd7b56
itslisek <80004169+itsmmigdal@users.noreply.github.com>
2022-04-03 16:07:46 +0200

    Update BaseGradeBook.cs

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -13,10 +13,13 @@ namespace GradeBook.GradeBooks
     {
         public string Name { get; set; }
         public List<Student> Students { get; set; }
+        public bool IsWeighted { get; set; }
+        public GradeBookType Type { get; set; }
 
         public BaseGradeBook(string name)
         {
             Name = name;
+            IsWeighted - isweighted;
             Students = new List<Student>();
         }
 
@@ -106,20 +109,21 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
-            switch (letterGrade)
+            int gpa = letterGrade switch
             {
-                case 'A':
-                    return 4;
-                case 'B':
-                    return 3;
-                case 'C':
-                    return 2;
-                case 'D':
-                    return 1;
-                case 'F':
-                    return 0;
+                'A' => 4,
+                'B' => 3,
+                'C' => 2,
+                'D' => 1,
+                  _ => 0,
+            };
+            
+            if (IsWieghted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
+            {
+                gpa++;
             }
-            return 0;
+            
+            return gpa;
         }
 
         public virtual void CalculateStatistics()
